bf98037a2ba1d295b45088e5cc269465deb0c0aa
Michael Schaefermeyer <mschae@users.noreply.github.com>
2014-07-28 15:55:45 +0200

    Refactor Directed.Message
    If we fix the order of the function definitions, we don't need the guard clause. Not a big deal but just nicer to read me thinks.

--- lib/directed/message.ex
+++ lib/directed/message.ex
@@ -1,12 +1,12 @@
 defmodule Directed.Message do
-  def emit(message, event_manager) when is_list(message) == false do
-    GenEvent.notify(event_manager, message)
-  end
-
   def emit([message | tail], event_manager) do
     emit(message, event_manager)
     emit(tail, event_manager)
   end
 
   def emit([], _event_manager), do: nil
+  
+  def emit(message, event_manager) do
+    GenEvent.notify(event_manager, message)
+  end
 end
